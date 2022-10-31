68dfe2d88c5c5aa2a46b8913aefe6ef34b3908f3
ahmadauli37 <116229479+ahmadauli37@users.noreply.github.com>
2022-10-31 08:49:15 +0700

    Add files via upload

---
+++ MODUL1_AULIA/src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Selamat datang di restoran EAD");
+        System.out.println("Silahkan register terlebih dahulu");
+        System.out.println("==================================");
+
+        Scanner tb = new Scanner (System.in);
+        System.out.println("NIM:");
+        int tubes = tb.nextInt();
+
+    }
+}
